8c783aaf280cefa04864aa5f2926bf586e12886c
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-01-16 12:01:09 +0530

    Time: 2 ms (13.41%) | Memory: 64.3 MB (36.18%) - LeetSync

--- 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
+++ 2533-bitwise-xor-of-all-pairings/bitwise-xor-of-all-pairings.cpp
@@ -3,14 +3,11 @@ public:
     int xorAllNums(vector<int>& nums1, vector<int>& nums2) {
         int result = 0;
 
-        // If nums2.size() is odd, XOR all elements of nums1
         if (nums2.size() % 2 == 1) {
             for (int num : nums1) {
                 result ^= num;
             }
         }
-
-        // If nums1.size() is odd, XOR all elements of nums2
         if (nums1.size() % 2 == 1) {
             for (int num : nums2) {
                 result ^= num;
