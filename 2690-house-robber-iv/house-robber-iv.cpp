f81ba9e6e1297df153d637a3b2da531002a11088
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-03-21 21:47:41 +0530

    Time: 23 ms (51.88%) | Memory: 61 MB (33.03%) - LeetSync

--- 2690-house-robber-iv/house-robber-iv.cpp
+++ 2690-house-robber-iv/house-robber-iv.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    bool canRob(vector<int>& nums, int k, int maxCap) {
-        int count = 0; // Number of houses robbed
-        int n = nums.size();
-        
-        for (int i = 0; i < n; i++) {
-            if (nums[i] <= maxCap) { 
-                count++; // Rob this house
-                i++; // Skip next house (constraint: can't rob adjacent houses)
-            }
-            if (count >= k) return true; // If we've robbed k houses, return true
-        }
-        return false;
-    }
-
     int minCapability(vector<int>& nums, int k) {
-        int left = *min_element(nums.begin(), nums.end()); // Min possible money
-        int right = *max_element(nums.begin(), nums.end()); // Max possible money
-        int result = right;
-
+        int left = nums[0], right = nums[0];
+        int n = nums.size();
+        for (int i = 1; i < n; i++) {
+            if (nums[i] > right) right = nums[i];
+            if (nums[i] < left) left = nums[i];
+        }
         while (left <= right) {
-            int mid = left + (right - left) / 2; // Middle value as potential max capability
-            if (canRob(nums, k, mid)) {
-                result = mid; // Try to find a smaller max capability
-                right = mid - 1;
-            } else {
-                left = mid + 1; // Increase capability to make it possible
+            int mid = (left + right) / 2;
+            if (check(nums, mid, k)) right = mid - 1;
+            else left = mid + 1;
+        }
+        return left;
+    }
+private:
+    bool check(vector<int>& nums, int guess, int k) {
+        int n = nums.size();
+        if (n == 1) return nums[0] <= guess;
+        int count = 0; 
+        for (int i = 0; i < n; i++) {
+            if (nums[i] <= guess) {
+                count++;
+                i++;
             }
         }
-        return result;
+        if (count >= k) return true;
+        return false;
     }
 };
