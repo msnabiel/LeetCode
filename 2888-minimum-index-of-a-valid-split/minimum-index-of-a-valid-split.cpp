e8766056bd748ee8065bc4e81570fd7da1ee9e07
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-03-27 19:28:31 +0530

    Time: 2 ms (84.02%) | Memory: 89.9 MB (70.90%) - LeetSync

--- 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
+++ 2888-minimum-index-of-a-valid-split/minimum-index-of-a-valid-split.cpp
@@ -1,13 +1,38 @@
 class Solution {
 public:
     int minimumIndex(vector<int>& nums) {
-        unordered_map <int,int> mpp , left;
-            for (int i : nums) mpp[i]++;
-            for (int i = 0 ; i < nums.size()-1; i++) {
-                left[nums[i]]++, mpp[nums[i]]--;
-                if (left[nums[i]] * 2 > (i + 1) && mpp[nums[i]] * 2 > (nums.size()-i-1))
-                return i;
-            }
-            return -1;
+        int cnt = 0;
+		int dom_num = 0;
+
+		for (int num : nums) {
+			if (cnt == 0) {
+				dom_num = num;
+			}
+			if (dom_num == num) {
+				cnt++;
+			} else {
+				cnt--;
+			}
+		}
+
+		int n = nums.size();
+		int total = count(nums.begin(), nums.end(), dom_num);
+
+		if (n == total * 2 - 1) {
+			return -1;
+		}
+
+		cnt = 0;
+		for (int i = 0; i < n; i++) {
+			if (nums[i] == dom_num) {
+				cnt++;
+			}
+
+			if (cnt * 2 > i + 1 && (total - cnt) * 2 > n - i - 1) {
+				return i;
+			}
+		}
+
+		return -1;
     }
 };
