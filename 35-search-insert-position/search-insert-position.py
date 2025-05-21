1bc0a133d899fef923399b383a1f1199e844bcf3
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-21 20:42:20 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (89.37%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def searchInsert(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        for i, num in enumerate(nums):
+            if num >= target:
+                return i
+        return len(nums)
+
+
+        
