5f180db505e63cd6ddd1e7872c79752c590f7826
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-22 21:36:08 +0530

    Time: 6 ms (5.19%) | Memory: 12.4 MB (80.64%) - LeetSync

---
+++ 231-power-of-two/power-of-two.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPowerOfTwo(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n <= 0:
+            return False
+        return 1 == str(bin(n)).count("1")
+
+
+        
