088d79300662bdac5622b6bbe48fa60c72ce8a1c
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-26 21:33:24 +0530

    Time: 221 ms (86.07%) | Memory: 13 MB (86.07%) - LeetSync

---
+++ 1456-find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance/find-the-city-with-the-smallest-number-of-neighbors-at-a-threshold-distance.py
@@ -0,0 +1,38 @@
+class Solution(object):
+    def findTheCity(self, n, edges, distanceThreshold):
+        """
+        :type n: int
+        :type edges: List[List[int]]
+        :type distanceThreshold: int
+        :rtype: int
+        """
+        # Step 1: Initialize distance matrix
+        dist = [[float('inf')] * n for _ in range(n)]
+        for i in range(n):
+            dist[i][i] = 0
+
+        for u, v, w in edges:
+            dist[u][v] = w
+            dist[v][u] = w
+
+        # Step 2: Floyd-Warshall - All pairs shortest path
+        for k in range(n):
+            for i in range(n):
+                for j in range(n):
+                    if dist[i][k] + dist[k][j] < dist[i][j]:
+                        dist[i][j] = dist[i][k] + dist[k][j]
+
+        # Step 3: Count reachable cities for each node
+        min_count = float('inf')
+        res = -1
+        for i in range(n):
+            count = 0
+            for j in range(n):
+                if i != j and dist[i][j] <= distanceThreshold:
+                    count += 1
+            # If tie, choose the city with the **greater index**
+            if count <= min_count:
+                min_count = count
+                res = i
+
+        return res
