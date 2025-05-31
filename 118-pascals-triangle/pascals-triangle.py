7498b46d1111968c6d5d1165e2da1ba8a0affec2
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-05-31 20:59:19 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (22.44%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -4,22 +4,16 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
-        result = []
-        if numRows == 0:
-            return result
+        L = []
 
-        first_row = [1]
-        result.append(first_row)
-
-        for i in range(1, numRows):
-            prev_row = result[i - 1]
-            current_row = [1]
-
-            for j in range(1, i):
-                current_row.append(prev_row[j - 1] + prev_row[j])
-
-            current_row.append(1)
-            result.append(current_row)
-
-        return result
+        for i in range(numRows):
+            if i == 0:
+                L.append([1])
+            else:
+                row = [1]
+                for j in range(1, i):
+                    row.append(L[i-1][j-1] + L[i-1][j])
+                row.append(1)
+                L.append(row)
         
+        return L
