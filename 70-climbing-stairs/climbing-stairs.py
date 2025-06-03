97f97a6dc16ad2ca53dd559030567e043b752cfa
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-06-03 15:37:24 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (80.64%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,11 +4,10 @@ class Solution(object):
         :type n: int
         :rtype: int
         """
-        if n <= 1:
-            return 1
-
-        a, b = 1, 1
-        for _ in range(2, n + 1):
+        if n <= 2:
+            return n
+        
+        a, b = 1, 2
+        for i in range(3, n + 1):
             a, b = b, a + b
         return b
-        
