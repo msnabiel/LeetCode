2bd562c9d3c889657f44f00d8f0a21f81ea52a2b
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-06-18 13:12:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.64%) - LeetSync

---
+++ 520-detect-capital/detect-capital.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def detectCapitalUse(self, word):
+        """
+        :type word: str
+        :rtype: bool
+        """
+        return (word.isupper() or
+                word.islower() or
+                word[0].isupper() and word[1:].islower())
