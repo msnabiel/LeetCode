59957545b25b6dbc2017e6808b0a65f4783480bd
msnabiel <89602212+msnabiel@users.noreply.github.com>
2025-06-18 13:20:27 +0530

    Time: 4 ms (31.62%) | Memory: 12.5 MB (74.36%) - LeetSync

---
+++ 1274-number-of-days-between-two-dates/number-of-days-between-two-dates.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def daysBetweenDates(self, date1, date2):
+        """
+        :type date1: str
+        :type date2: str
+        :rtype: int
+        """
+        from datetime import datetime
+        """
+        :type date1: str
+        :type date2: str
+        :rtype: int
+        """
+        d1 = datetime.strptime(date1, "%Y-%m-%d")
+        d2 = datetime.strptime(date2, "%Y-%m-%d")
+        return abs((d2 - d1).days)
